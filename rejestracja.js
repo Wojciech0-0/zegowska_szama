c02482f90993e988262c84a71fc6eb3371e95ab9
ErykBaron <baroneryk44@gmail.com>
2026-05-07 13:11:48 +0200

    Add files via upload

---
+++ rejestracja.js
@@ -0,0 +1,7 @@
+document.getElementById('register_button').addEventListener("click", (e) => {
+    e.preventDefault();
+
+    //jezeli wartosci imie nazwisko email haslo i haslo 2 nie sa wpisane wyswietl blad.
+    // haslo 1 musi byc takie same jak haslo2 jezeli nie wyswietl blad
+    // jezeli rejestraca poszla zgodnie z planem przenies uzytkownika na stone logowanie
+})
---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('pytania').addEventListener("click", (e) => {
+            e.preventDefault();
+
+            window.location.href = "pytania.html";
+        })
+
+document.getElementById('stan_magazynu').addEventListener("click", (e) => {
+            e.preventDefault();
+
+            window.location.href = "magazyn.html";
+        })
+
+document.getElementById('szczegoly').addEventListener("click", (e) => {
+            e.preventDefault();
+
+            window.location.href = "szczegoly.html";
+        })
+
+document.getElementById('historia').addEventListener("click", (e) => {
+            e.preventDefault();
+
+            window.location.href = "historia.html";
+        })
