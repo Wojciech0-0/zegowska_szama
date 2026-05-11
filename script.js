4f9c9a4bf04937459aeaabee073d8da01958335d
ErykBaron <baroneryk44@gmail.com>
2026-05-11 13:01:22 +0200

    Add files via upload
    dodanie paru zmian i strony szczegoly

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-document.getElementById('pytania').addEventListener("click", (e) => {
-            e.preventDefault();
+document.querySelector('.pytania-btn').addEventListener("click", (e) => {
 
-            window.location.href = "pytania.html";
-        })
+    e.preventDefault();
 
-document.getElementById('stan_magazynu').addEventListener("click", (e) => {
-            e.preventDefault();
+    window.location.href = "pytania.html";
 
-            window.location.href = "magazyn.html";
-        })
+});
 
-document.getElementById('szczegoly').addEventListener("click", (e) => {
-            e.preventDefault();
 
-            window.location.href = "szczegoly.html";
-        })
+document.querySelector('.magazyn-btn').addEventListener("click", (e) => {
 
-document.getElementById('historia').addEventListener("click", (e) => {
-            e.preventDefault();
+    e.preventDefault();
 
-            window.location.href = "historia.html";
-        })
+    window.location.href = "magazyn.html";
+
+});
+
+
+document.querySelector('.szczegoly-btn').addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    window.location.href = "szczegoly.html";
+
+});
+
+
+document.querySelector('.historia-btn').addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    window.location.href = "historia.html";
+
+});
+
+
+document.querySelectorAll('.details-btn').forEach(button => {
+
+    button.addEventListener('click', (e) => {
+
+        e.preventDefault();
+
+        window.location.href = "szczegoly.html";
+
+    });
+
+});
+
+
+document.querySelectorAll('.status-btn').forEach(button => {
+
+    button.addEventListener('click', (e) => {
+
+        e.preventDefault();
+
+        window.location.href = "szczegoly.html";
+
+    });
+
+});
